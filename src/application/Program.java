b1395b4ce346fb32b5bfaf8ed1d21db7820d1570
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-11-11 20:17:39 +0100

    Function - Part 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,6 @@ import util.UpperCaseName;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
@@ -16,7 +15,7 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        List<String> names = list.stream().map(new UpperCaseName()).toList();
+        List<String> names = list.stream().map(Product::staticUpperCaseName).toList();
 
         names.forEach(System.out::println);
     }
