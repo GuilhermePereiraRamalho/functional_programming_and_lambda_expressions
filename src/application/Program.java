9379496fe914607615761d3993f295d548110b54
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-11-11 20:04:33 +0100

    Consumer - Part 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import entities.Product;
+import util.PriceUpdate;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
@@ -15,12 +15,8 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        double min = 100.0;
-        
-        list.removeIf(p -> p.getPrice() >= min);
+        list.forEach(new PriceUpdate());
 
-        for (Product p : list){
-            System.out.println(p);
-        }
+        list.forEach(System.out::println);
     }
 }
