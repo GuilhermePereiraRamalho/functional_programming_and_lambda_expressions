f7d7f70de25aa40402bda8b75eb85c49536d9549
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-11-11 20:28:50 +0100

    Creating functions that receive functions as parameters

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,10 @@
 package application;
 
 import entities.Product;
-import util.UpperCaseName;
+import services.ProductService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 public class Program {
     public static void main(String[] args) {
@@ -16,8 +15,11 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).toList();
+        ProductService ps = new ProductService();
 
-        names.forEach(System.out::println);
+        double sum = ps.filteredSum(list, p -> p.getPrice() < 100.0);
+
+        System.out.println("Sum = "+ String.format("%.2f", sum));
+
     }
 }
